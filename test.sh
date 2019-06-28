fcc3550f334e73e0b014c40d54f9e9ed49a4f3d3
Jon Graf <jongraf1@gmail.com>
2019-06-28 10:00:41 -0500

    extracted user and pass to variables

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ function sleepBeforeMigration {
 function dockerMigration {
     echo "-----Migrating Docker Images-----"
     docker exec $SQL_DOCKER_NAME ./opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SQL_PASS -Q "CREATE DATABASE PlanManagement;"
-#    docker run -i msSql-flyway -url="jdbc:sqlserver://$SQL_DOCKER_NAME:$SQL_PORT;instanceName=SQLEXPRESS;databaseName=PlanManagement;" -locations="filesystem:/var/flyway" -user=sa -password=Password123! -connectRetries=10 migrate
+#    docker run -i msSql-flyway -url="jdbc:sqlserver://$SQL_DOCKER_NAME:$SQL_PORT;instanceName=SQLEXPRESS;databaseName=PlanManagement;" -locations="filesystem:/var/flyway" -user=$SQL_USER -password=$SQL_PASS -connectRetries=10 migrate
     docker run --net mssqlservertest_default --rm -v C:\\Users\\g825714\\MyProjects\\MsSqlServerTest\\docker\\flyway\\db\\migration:/flyway/sql boxfuse/flyway:5.2.4 -url="jdbc:sqlserver://$SQL_DOCKER_NAME:$SQL_PORT;databaseName=PlanManagement;" -user=$SQL_USER -password=$SQL_PASS migrate
 }
 
