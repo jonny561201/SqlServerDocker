0d2b613e95f0fb83103a5f5c3fa1c83fbc042619
Jon Graf <jongraf1@gmail.com>
2019-06-28 09:52:23 -0500

    extracted more variables for docker migration script

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 SQL_PASS=Password123!
+SQL_USER=sa
 SQL_DOCKER_NAME="msSql-test"
+SQL_PORT=1433
 
 
 function sleepBeforeMigration {
@@ -12,13 +14,13 @@ function sleepBeforeMigration {
 function dockerMigration {
     echo "-----Migrating Docker Images-----"
     docker exec $SQL_DOCKER_NAME ./opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SQL_PASS -Q "CREATE DATABASE PlanManagement;"
-#    docker run -i msSql-flyway -url="jdbc:sqlserver://$SQL_DOCKER_NAME:1433;instanceName=SQLEXPRESS;databaseName=PlanManagement;" -locations="filesystem:/var/flyway" -user=sa -password=Password123! -connectRetries=10 migrate
-    docker run --net mssqlservertest_default --rm -v C:\\Users\\g825714\\MyProjects\\MsSqlServerTest\\docker\\flyway\\db\\migration:/flyway/sql boxfuse/flyway:5.2.4 -url="jdbc:sqlserver://$SQL_DOCKER_NAME:1433;databaseName=PlanManagement;" -user=sa -password=$SQL_PASS migrate
+#    docker run -i msSql-flyway -url="jdbc:sqlserver://$SQL_DOCKER_NAME:$SQL_PORT;instanceName=SQLEXPRESS;databaseName=PlanManagement;" -locations="filesystem:/var/flyway" -user=sa -password=Password123! -connectRetries=10 migrate
+    docker run --net mssqlservertest_default --rm -v C:\\Users\\g825714\\MyProjects\\MsSqlServerTest\\docker\\flyway\\db\\migration:/flyway/sql boxfuse/flyway:5.2.4 -url="jdbc:sqlserver://$SQL_DOCKER_NAME:$SQL_PORT;databaseName=PlanManagement;" -user=$SQL_USER -password=$SQL_PASS migrate
 }
 
 function dockerCompose {
     echo "-----Starting Up Docker Images-----"
-#    docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SQL_PASS" -e "MSSQL_PID=Express" --name $SQL_DOCKER_NAME -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu
+#    docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SQL_PASS" -e "MSSQL_PID=Express" --name $SQL_DOCKER_NAME -p $SQL_PORT:$SQL_PORT -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu
     docker-compose up -d
 }
 
