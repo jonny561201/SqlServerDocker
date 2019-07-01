f439a9b5f5e6a11af1d4346deedaad025b6087e3
Jon Graf <jongraf1@gmail.com>
2019-07-01 10:52:54 -0500

    updated flyway container to use dynamic relative pathing for volume

--- test.sh
+++ test.sh
@@ -2,27 +2,31 @@
 
 SQL_PASS=Password123!
 SQL_USER=sa
-SQL_DOCKER_NAME="msSql-test"
+SQL_DOCKER_NAME="MsSqlTest"
 SQL_PORT=1433
+PRESENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 function sleepBeforeMigration {
-    echo "-----Sleeping Before Migration-----"
+    echo "----------Sleeping Before Migration----------"
     sleep 12
 }
 
 function dockerMigration {
-    echo "-----Migrating Docker Images-----"
+    echo "----------Creating PlanManagement Database----------"
     docker exec $SQL_DOCKER_NAME ./opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SQL_PASS -Q "CREATE DATABASE PlanManagement;"
-#    docker run -i msSql-flyway -url="jdbc:sqlserver://$SQL_DOCKER_NAME:$SQL_PORT;instanceName=SQLEXPRESS;databaseName=PlanManagement;" -locations="filesystem:/var/flyway" -user=$SQL_USER -password=$SQL_PASS -connectRetries=10 migrate
-    docker run --net mssqlservertest_default --rm -v C:\\Users\\g825714\\MyProjects\\MsSqlServerTest\\docker\\flyway\\db\\migration:/flyway/sql boxfuse/flyway:5.2.4 -url="jdbc:sqlserver://$SQL_DOCKER_NAME:$SQL_PORT;databaseName=PlanManagement;" -user=$SQL_USER -password=$SQL_PASS migrate
+
+    DOCKER_VOLUME="$PRESENT_DIR\\docker\\flyway\\db\\migration"
+    echo "----------Flyway Volume set to: $DOCKER_VOLUME"
+    echo "----------Executing Flyway Migrations----------"
+    docker run --net mssqlservertest_default --rm -v /${DOCKER_VOLUME}:/flyway/sql boxfuse/flyway:5.2.4 -url="jdbc:sqlserver://$SQL_DOCKER_NAME:$SQL_PORT;databaseName=PlanManagement;" -user=$SQL_USER -password=$SQL_PASS migrate
 }
 
 function dockerCompose {
-    echo "-----Starting Up Docker Images-----"
-#    docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SQL_PASS" -e "MSSQL_PID=Express" --name $SQL_DOCKER_NAME -p $SQL_PORT:$SQL_PORT -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu
+    echo "----------Starting Up Docker Images----------"
     docker-compose up -d
 }
+
 
 dockerCompose
 sleepBeforeMigration
